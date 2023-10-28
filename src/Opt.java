92ea4b42e38e9bae23cc479cc851728af841cc83
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-28 07:10:20 -0700

    second commit

---
+++ src/Opt.java
@@ -0,0 +1,8 @@
+public class Opt {
+    public double div() {
+        double num1 = Math.random();
+        double num2 = Math.random();
+        double r = num1 / num2;
+        return r;
+    }
+}
