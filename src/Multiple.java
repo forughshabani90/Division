18d360df43b9d5ce8dc30b9aed42d81551d1d28b
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-28 08:23:08 -0700

    fc

---
+++ src/Multiple.java
@@ -0,0 +1,8 @@
+public class Multiple {
+    public double Multiple(){
+        double n1=Math.random();
+        double n2=Math.random();
+        double result=n1 * n2;
+        return result;
+    }
+}
