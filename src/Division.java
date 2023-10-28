ffe362e8b15824c61accd7507aed04a941372b58
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-28 06:53:20 -0700

    first commit

---
+++ src/Division.java
@@ -0,0 +1,9 @@
+public class Division {
+
+    public static void main(String[] args) {
+        double num1 = Math.random();
+        double num2 = Math.random();
+        double r = num1 / num2;
+        System.out.println(r);
+    }
+}
